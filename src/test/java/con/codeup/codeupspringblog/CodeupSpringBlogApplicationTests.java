4ad2c5b0a474686cd2c3f471b006d3f586624506
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-05-17 09:50:42 -0500

    commit all

---
+++ src/test/java/con/codeup/codeupspringblog/CodeupSpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package con.codeup.codeupspringblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeupSpringBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
